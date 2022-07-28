2c7e0011cd909061186ddc90efa1f0421a08cea1
Jau2002 <idamendjaume@gmail.com>
2022-07-28 11:15:03 -0500

    fix(eslint): indifference of any types
    A bug occurred because Typescript by default parses void types, in test callback functions. It's imperceptible to Jest's view, it checks 10/10 at runtime, but Eslint's type difference needs leastways "any".

--- src/tests/clousures.spec.ts
+++ src/tests/clousures.spec.ts
@@ -1,17 +1,18 @@
 import counter from '../closures_recursion/closures';
+import accountant from '../types';
 
-describe('counter', () => {
-	it('should return a function', () => {
+describe('counter', (): any => {
+	it('should return a function', (): any => {
 		expect(typeof counter).toBe('function');
 	});
-	it('should increment the number when the function is invoked', () => {
-		const countFunc: any = counter();
+	it('should increment the number when the function is invoked', (): any => {
+		const countFunc: accountant = counter();
 		expect(countFunc()).toBe(1);
 		expect(countFunc()).toBe(2);
 	});
-	it('should have two different accumulators if two counters are create', () => {
-		const countOne: any = counter();
-		const countTwo: any = counter();
+	it('should have two different accumulators if two counters are create', (): any => {
+		const countOne: accountant = counter();
+		const countTwo: accountant = counter();
 		expect(countOne()).toBe(1);
 		expect(countOne()).toBe(2);
 		expect(countOne()).toBe(3);
--- src/tests/converter.spec.ts
+++ src/tests/converter.spec.ts
@@ -1,23 +1,23 @@
 import { binarioDecimal, decimalBinario } from '../number_systems/converter';
 
-describe('binary to decimal', () => {
+describe('binary to decimal', (): any => {
 	const [resultOne, resultTwo] = [2, 7];
 	const [actualOne, actualTwo] = ['10', '111'];
-	it('should return 2', () => {
+	it('should return 2', (): any => {
 		expect(binarioDecimal(actualOne)).toBe(resultOne);
 	});
-	it('should return 7', () => {
+	it('should return 7', (): any => {
 		expect(binarioDecimal(actualTwo)).toBe(resultTwo);
 	});
 });
 
-describe('decimal to binary', () => {
+describe('decimal to binary', (): any => {
 	const [actualOne, actualTwo] = [4, 7];
 	const [resultOne, resultTwo] = ['100', '111'];
-	it('should return "100"', () => {
+	it('should return "100"', (): any => {
 		expect(decimalBinario(actualOne)).toBe(resultOne);
 	});
-	it('should return "111"', () => {
+	it('should return "111"', (): any => {
 		expect(decimalBinario(actualTwo)).toBe(resultTwo);
 	});
 });
